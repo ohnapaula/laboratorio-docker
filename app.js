d563413891f83bc9124db901358247f22a29aff8
ohnapaula <ohnapaula@gmail.com>
2025-11-02 12:34:24 -0300

    refactor: Melhoria na estrutura

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: 5432,
 });
 
 function gerarHTML(mensagem, cor) {
